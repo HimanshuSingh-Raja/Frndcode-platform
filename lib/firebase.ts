5899224638e8863b47bc3154804002d61a913f7c
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2026-06-22 01:25:41 +0530

    Setup Firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUQdqx3vZidyJs6Xtlq04NZzCKpbdfKao",
+  authDomain: "frndcode-platform.firebaseapp.com",
+  projectId: "frndcode-platform",
+  storageBucket: "frndcode-platform.firebasestorage.app",
+  messagingSenderId: "976308387289",
+  appId: "1:976308387289:web:3c427f702f3812f4dcedc8",
+  measurementId: "G-T714SE5PQ2",
+};
+
+export const app = initializeApp(firebaseConfig);
